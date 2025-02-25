696a7df9935d4d091d9b4d6c55022736e187649f
AnasLabzar <77747369+AnasLabzar@users.noreply.github.com>
2025-02-25 03:04:07 +0000

    Update index.tsx

--- src/components/AddInvoice/index.tsx
+++ src/components/AddInvoice/index.tsx
@@ -298,7 +298,7 @@ const AddInvoice = () => {
             const orderIds = await Promise.all(
                 items.map(async (item) => {
                     const orderData = {
-                        refOrder: generateOrderRef(),
+                        refOrder: generateOrderRef(item.customerId),
                         totalProfit: calculateTotalProfit(item.profit, item.quantity),
                         ordred: new Date(ordredDate).toISOString(),
                         dueDate: new Date(dueDate).toISOString(),
